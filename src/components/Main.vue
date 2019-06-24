1a4faa09e3e04167a2bea772b8ae1797469bff09
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-06-24 17:48:03 +0200

    Added top product component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-
+    <TopProduct v-bind:item ='items.items[0]' />
     <p> Welcome to WindeDB</p>
     <img src="https://www.wsjwine.com/images/us/wsj/homepage/sliders/2018/november/wsj_185off_Q2Digital_mobile_bottom.jpg"/>
   </div>
 </template>
 
 <script>
+import TopProduct from './TopProduct.vue'
 import axios from 'axios'
+
 export default {
   name: 'Main',
   components: {
+    TopProduct
   },
   data() {
     return {
       // "link" is a public GitHub Gist for anyone to see
       link: "https://gist.githubusercontent.com/ThomasStorli/91432dde678216ecab41c31ae007f49b/raw/3a0b8fde6c58c5ae5463c30ac7e0ccf45d9f48c4/wine-db-data.json",
-      items: null
+      items: null,
+      testDat: ["Hei", "På", "Dei"]
     }
   },
   methods: {
---
+++ src/components/TopProduct.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <footer>
+      <h1> {{item.name}} </h1>
+      <!--<img src="{{item.name}}" />-->
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TopProduct',
+  props: [
+    'item'
+  ]
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 1em;
+  text-align: center;
+}
+
+img {
+  width: 50%;
+}
+</style>
