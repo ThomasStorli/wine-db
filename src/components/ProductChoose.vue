1cb8102ad049041eedb95f01fb2263d303edefdc
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-06-25 15:33:09 +0200

    Update ProductChoose.vue

--- src/components/ProductChoose.vue
+++ src/components/ProductChoose.vue
@@ -1,23 +1,24 @@
 <template>
   <form>
     <div class="container">
-    <div class="row pb-2">
+      <div class="row pb-2">
+        <div class="col">
+          <select class="form-control" id="exampleFormControlSelect1" v-model="type">
+            <option>Alle typer alkohol</option>
+            <option>Rødvin</option>
+            <option>Hvitvin</option>
+            <option>Bobler</option>
+            <option>Brennevin</option>
+            <option>Øl</option>
+          </select>
+        </div>
       <div class="col">
-      <select class="form-control" id="exampleFormControlSelect1">
-        <option>Alle typer alkohol</option>
-        <option>Vin</option>
-        <option>Brennevin</option>
-        <option>Whiskey</option>
-        <option>Øl</option>
-      </select>
-    </div>
-    <div class="col">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        18+
-      </label>
-    </div>
-    </div>
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">
+          18+
+        </label>
+      </div>
+      </div>
     </div>
   </form>
 </template>
