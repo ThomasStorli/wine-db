a6b0d0271e7fa616eae523d2314bc2fa701684a4
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-06-27 19:32:46 +0200

    The even more condensed list is used

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   data() {
     return {
       // "link" is a public GitHub Gist for anyone to see
-      link: "https://gist.githubusercontent.com/ThomasStorli/91432dde678216ecab41c31ae007f49b/raw/",
+      link: "https://gist.githubusercontent.com/AlexanderDeBattista/c93e0407cfe939d0a77137cc8ad210f8/raw",
       test_link: "https://gist.githubusercontent.com/ThomasStorli/c36723fbc0ed7c2368f30eed186d53b4/raw/e55586abe9c3e71a0e8ded1d0686630b337d3287/gistfile1.json",
       items: null
     }
