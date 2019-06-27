0243801c5c17cfbae56be64cb772ce38a07dd115
Thomas Storli <thomas.storli@live.no>
2019-06-27 19:37:32 +0200

    Removed sorting options

--- src/components/ProductChoose.vue
+++ src/components/ProductChoose.vue
@@ -8,23 +8,6 @@
             <option v-for="(type, index) in types" :key="index" :value="type">{{type}}</option>
         </select>
       </div>
-      <div class="form-row">
-        <div class="col">
-          <label for="minABV"> Minimum ABV </label>
-          <input id="minABV" type="number" class="form-control" value="0" min="0" max="60" step="1"/>
-        </div>
-        <div class="col">
-          <label for="maxABV"> Maks ABV </label>
-          <input id="maxABV" type="number" class="form-control" value="60" min="0" max="60" step="1"/>
-        </div>
-        <div class="col">
-          <label for="agelimit"> Aldersgrense </label>
-          <select id="agelimit" class="form-control">
-              <option value="18">18+</option>
-              <option value="20" selected="selected">20+</option>
-          </select>
-        </div>
-      </div>
     </form>
   </div>
 </template>
