126fdc88846284b5837efed32604572870f62730
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-07-01 11:11:03 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <footer>
-      <h1> WineDB </h1>
+      <h3 class="text-center">AlkisKalkis.no er ikke tilknyttet til Vinmonopolet på noen måte, og vi tar forbehold om at feil kan forekomme</h3>
+      <h1> AlkisKalkis - {{ new Date().getFullYear() }} </h1>
     </footer>
   </div>
 </template>
@@ -19,6 +20,9 @@ export default {
 h1 {
   font-size: 1em;
   text-align: center;
+}
+h3{
+  font-size: 0.9em;
 }
 
 footer {
