79b7eff1255ac81f5664e83ce48750b376951d31
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-07-21 12:37:06 +0200

    Added comments, and you can now push changes directly to my gist

--- scraper/UpdateGist.py
+++ scraper/UpdateGist.py
@@ -1,10 +1,10 @@
 import github
 import codecs
-import json
 
+# Pushes the output.json file to a gist on github
 def update():
-    #Github token or login
-    g = github.Github("-")
+    #Connects to a github acount
+    g = github.Github("6e9298305c669d73192ae19c3d972f10841dba80")
+    # Add the file to a gist
     with codecs.open('output.json', 'r', encoding='utf8') as f:
-        #Gist ID
-        g.get_gist("-").edit(files = {"BestCheapestDrink.json" : github.InputFileContent(f.read())})
+        g.get_gist("c93e0407cfe939d0a77137cc8ad210f8").edit(files = {"BestCheapestDrink.json" : github.InputFileContent(f.read())})
