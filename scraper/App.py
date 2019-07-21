24c464ce48c5060583a46bda9d2ab51a6d5f07cb
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2019-07-21 12:33:19 +0200

    Added more categories and added a category counter

--- scraper/App.py
+++ scraper/App.py
@@ -1,22 +1,26 @@
 import json
 import codecs
 from datetime import date
+import operator
 
 def start():
-    with codecs.open('wine_sorted.json', 'r', encoding='utf8') as f:
+    with codecs.open('WineCsvSorted.json', 'r', encoding='utf8') as f:
         data = json.load(f)
         createObject(data)
 
-types = ["Rødvin", "Cognac", "Hvitvin", "Rosévin", "Sterkvin", "Musserende vin", "Brennevin", "Vodka", "Gin", "Sake", "Øl", "Perlende vin", "Tequila", "Bitter", "Rom", "Likør", "Whisky", "Akevitt", "India pale ale"]
+types = ["Rødvin", "Cognac", "Hvitvin", "Rosévin", "Sterkvin", "Musserende vin", "Brennevin", "Vodka", "Gin", "Sake", "Øl", "Perlende vin", "Tequila", "Bitter", "Rom", "Likør", "Whisky", "Akevitt", "Sider", "Surøl", "Portvin", "Rosévin"]
+
+
 def createObject(s):
     prodList = []
     prodList.append({"name": "top", "products": []})
 
     for p in s:
-        if p not in prodList[0]["products"] and len(prodList[0]["products"])< 100:
+        if p not in prodList[0]["products"] and len(prodList[0]["products"]) < 100:
             prodList[0]["products"].append(p)
-            
+
         for t in p["type"]:
+            if not isinstance(t, str): continue
             t = t.lower()
             t = t.capitalize()
             if t not in types:
@@ -27,7 +31,7 @@ def createObject(s):
             if (e == -1):
                 prodList.append({"name": t, "products": [p]})
                 continue
-            elif p not in prodList[e]["products"] and len(prodList[e]["products"])< 100:
+            elif p not in prodList[e]["products"] and len(prodList[e]["products"]) < 100:
                 prodList[e]["products"].append(p)
 
     today = date.today()
@@ -38,6 +42,8 @@ def createObject(s):
 
     with codecs.open("output.json", "w+", encoding='utf8') as g:
         json.dump(output, g, ensure_ascii=False)
+        g.close()
+
 
 def typeInList(l, t):
     for e, x in enumerate(l):
@@ -45,4 +51,19 @@ def typeInList(l, t):
             return e
     return -1
 
-start()
+
+#start()
+
+def counter():
+    with codecs.open('WineCsvSorted.json', 'r', encoding='utf8') as f:
+        data = json.load(f)
+    typer = {}
+    for i in data:
+        for j in i["type"]:
+            if j in typer:
+                typer[j] = typer[j] + 1
+            else:
+                typer[j] = 1
+    print(sorted(typer.items(), key=operator.itemgetter(1)))
+
+#counter()
